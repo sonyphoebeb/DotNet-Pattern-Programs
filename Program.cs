490693e650035f4391325259531bf9997e2fc7ec
sonyphoebeb <sonyphoebebaree@gmail.com>
2026-03-04 23:54:56 +0530

    Added Inverted Right-Angled Triangle programs using nested loops in C#

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ class Program
 {
     static void Main()
     {
+        // This is a simple pattern printing program that prints a right-angled triangle of asterisks.
         for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        //inverse right-angled triangle
+        for (int i = 5; i >= 1; i--)
         {
             for (int j = 1; j <= i; j++)
             {
