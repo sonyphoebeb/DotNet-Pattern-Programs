0541d625496dbf6266edafbb559642da2065f5e0
sonyphoebeb <sonyphoebebaree@gmail.com>
2026-03-04 23:59:01 +0530

    Added Inverted Pyramid star pattern program

--- Program.cs
+++ Program.cs
@@ -49,5 +49,25 @@ class Program
 
             Console.WriteLine();
         }
+
+        Console.WriteLine();
+
+        // inverse pyramid pattern
+        int num = 5;
+
+        for (int i = num; i >= 1; i--)
+        {
+            for (int j = 1; j <= num - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 1; k <= (2 * i - 1); k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
