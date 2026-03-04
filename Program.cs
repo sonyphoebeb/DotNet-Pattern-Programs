070be5ae688a57d63c48b3377d8461dbb5ae9cef
sonyphoebeb <sonyphoebebaree@gmail.com>
2026-03-04 23:52:51 +0530

    Added right-angled triangle star pattern program using nested loops in C#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DotnetPatterns;
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
